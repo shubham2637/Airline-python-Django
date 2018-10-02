a9937c9934d0997ac3d013eb1dcd60e371a535cc
shubham2637 <shubham37@outlook.com>
2018-10-02 22:47:07 +0530

    Day 1..

--- flight/views.py
+++ flight/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+from .models import Flight
+
 
 def index(request):
-    return HttpResponse("Flights")
+    context = {
+        "flights": Flight.objects.all()
+    }
+
+    return render(request,"flight/index.html", context)
 
